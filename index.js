9826487d05c09c584e309cd2014952cdbc5d84a6
André <andreglock@gmail.com>
2021-05-31 14:55:40 +0200

    done

---
+++ index.js
@@ -0,0 +1,52 @@
+class Component {
+    constructor(name) {
+        this.name = name;
+    }
+
+    render() { 
+        console.log(`<p>Component ${this.name}  rendering`);
+    }
+}
+
+class UserCard extends Component {
+    constructor(user) {
+        super(user);
+    }
+
+    render() {
+        console.log(`
+        <div class="card card-user">
+            <img class="card-img-top" src="user imageUrl here" alt>
+            <div class="card-body">
+                <h5 class="card-title">${this.name.user}</h5>
+                <p class="card-text">To contact, please send a message to</p>
+                <a
+                    href="mailto:${this.name.email}"
+                    class="btn btn-primary"
+                >
+                    ${this.name.email}
+                </a>
+            </div>
+        </div>`);
+    }
+}
+
+const usersArray = [
+    {user: 'Bob', email: 'bob@email.com'},
+    {user: 'Linda', email: 'linda@email.com'},
+    {user: 'Angela', email: 'angela@email.com'},
+    {user: 'Alaska', email: 'alaska@email.com'},
+    {user: 'Eureka', email: 'eureka@email.com'},
+]
+
+const users = usersArray.map(x => {
+    return new UserCard(x);
+})
+
+console.log(users);
+users.forEach(x => {
+    x.render();
+})
+
+console.log(typeof UserCard);
+console.log(typeof users[0]);
